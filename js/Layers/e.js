32da3c8944f372e81750a0306dbe2e72910e619a
Dressyapper <anthonykaraki9@gmail.com>
2026-09-14 07:14:44 +1000

    fix

--- js/Layers/e.js
+++ js/Layers/e.js
@@ -34,7 +34,7 @@ addLayer("E", {
     },
     color: "rgb(0, 255, 255)",
     requires() {
-        let req = new Decimal(1)
+        let req = new Decimal(150000)
         return req
     }, // Can be a function that takes requirement increases into account
     resource: "E", // Name of prestige currency
